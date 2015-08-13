ea4dcd3c00906eccbf6ec9a38e395b277a9a147c
Francisco Massa <fvsmassa@gmail.com>
2015-08-13 17:22:52 +0200

    Updating BatchProviderROI

--- nnf.lua
+++ nnf.lua
@@ -1,14 +1,15 @@
 require 'nn'
 require 'image'
-require 'inn'
+--require 'inn'
 require 'xlua'
 
 nnf = {}
 
 torch.include('nnf','DataSetPascal.lua')
 torch.include('nnf','BatchProvider.lua')
+torch.include('nnf','BatchProviderROI.lua')
 
-torch.include('nnf','SPP.lua')
+--torch.include('nnf','SPP.lua')
 torch.include('nnf','RCNN.lua')
 
 torch.include('nnf','Trainer.lua')
