bbe3dd20f4071422844780ef4b0946faf06af230
fsuzanomassa <fvsmassa@gmail.com>
2015-08-18 18:09:54 +0200

    Improve training script for frcnn

--- test_frcnn.lua
+++ test_frcnn.lua
@@ -1,10 +1,18 @@
 require 'nnf'
 
 dt = torch.load('pascal_2007_train.t7')
-ds = nnf.DataSetPascal{image_set='train',
-                       datadir='/home/francisco/work/datasets/VOCdevkit',
-                       roidbdir='/home/francisco/work/datasets/rcnn/selective_search_data'
-                       }
+if false then
+  ds = nnf.DataSetPascal{image_set='train',
+                         datadir='/home/francisco/work/datasets/VOCdevkit',
+                         roidbdir='/home/francisco/work/datasets/rcnn/selective_search_data'
+                        }
+else
+  ds = nnf.DataSetPascal{image_set='train',
+                         datadir='datasets/VOCdevkit',
+                         roidbdir='data/selective_search_data'
+                         }
+end
+
 if false then
   ds.roidb = {}
   for i=1,ds:size() do
@@ -15,9 +23,9 @@ else
   ds.roidb = dt.roidb
 end
 
-if false then
-bp = nnf.BatchProviderROI(ds)
-bp:setupData()
+if true then
+  bp = nnf.BatchProviderROI(ds)
+  bp:setupData()
 else
   bp = nnf.BatchProviderROI(ds)
   local temp = torch.load('pascal_2007_train_bp.t7')
@@ -28,52 +36,89 @@ end
 -- model
 ---------------------------------------------------------------------------------------
 do
+
   model = nn.Sequential()
   local features = nn.Sequential()
   local classifier = nn.Sequential()
-  
-  features:add(nn.SpatialConvolutionMM(3,96,11,11,4,4,5,5))
-  features:add(nn.ReLU(true))
-  features:add(nn.SpatialConvolutionMM(96,128,5,5,2,2,2,2))
-  features:add(nn.ReLU(true))
-  features:add(nn.SpatialMaxPooling(2,2,2,2))
 
-  classifier:add(nn.Linear(128*7*7,1024))
-  classifier:add(nn.ReLU(true))
-  classifier:add(nn.Dropout(0.5))
-  classifier:add(nn.Linear(1024,21))
+  if false then
+    features:add(nn.SpatialConvolutionMM(3,96,11,11,4,4,5,5))
+    features:add(nn.ReLU(true))
+    features:add(nn.SpatialConvolutionMM(96,128,5,5,2,2,2,2))
+    features:add(nn.ReLU(true))
+    features:add(nn.SpatialMaxPooling(2,2,2,2))
+
+    classifier:add(nn.Linear(128*7*7,1024))
+    classifier:add(nn.ReLU(true))
+    classifier:add(nn.Dropout(0.5))
+    classifier:add(nn.Linear(1024,21))
+  
+  else
+    require 'loadcaffe'
+--    local rcnnfold = '/home/francisco/work/libraries/rcnn/'
+--    local base_model = loadcaffe.load(
+--          rcnnfold..'model-defs/pascal_finetune_deploy.prototxt',
+--          rcnnfold..'data/caffe_nets/finetune_voc_2012_train_iter_70k',
+--    'cudnn')
+
+  local rcnnfold = '/home/francisco/work/libraries/caffe/examples/imagenet/'
+  local base_model = loadcaffe.load(
+                rcnnfold..'imagenet_deploy.prototxt',
+                rcnnfold..'caffe_reference_imagenet_model',
+                'cudnn')
+
+
+    for i=1,14 do
+      features:add(base_model:get(i):clone())
+    end
+
+    for i=17,22 do
+      classifier:add(base_model:get(i):clone())
+    end
+    classifier:add(nn.Linear(4096,21):cuda())
+    
+    collectgarbage()
+  end
+  collectgarbage()
 
   local prl = nn.ParallelTable()
   prl:add(features)
   prl:add(nn.Identity())
   model:add(prl)
-  model:add(nnf.ROIPooling(7,7):setSpatialScale(1/16))
+  model:add(nnf.ROIPooling(6,6):setSpatialScale(1/16))
+  --model:add(inn.ROIPooling(6,6):setSpatialScale(1/16))
   model:add(nn.View(-1):setNumInputDims(3))
   model:add(classifier)
 
 end
-
+print(model)
 parameters,gradParameters = model:getParameters()
 
-optimState = {learningRate = 1e-2, weightDecay = 0.0005, momentum = 0.9,
+optimState = {learningRate = 1e-3, weightDecay = 0.0005, momentum = 0.9,
               learningRateDecay = 0}
 
 --------------------------------------------------------------------------
 -- training
 --------------------------------------------------------------------------
 
-model:float()
+model:cuda()
 model:training()
 
-criterion = nn.CrossEntropyCriterion():float()
+criterion = nn.CrossEntropyCriterion():cuda()
 
-max_iter = 20
+display_iter = 20
+
+inputs = {torch.CudaTensor(),torch.FloatTensor()}
+target = torch.CudaTensor()
 
 function train()
   local err = 0
-  for i=1,max_iter do
-    xlua.progress(i,max_iter)
-    inputs,target = bp:getBatch(inputs,target)
+  for i=1,display_iter do
+    xlua.progress(i,display_iter)
+    inputs0,target0 = bp:getBatch(inputs0,target0)
+    inputs[1]:resize(inputs0[1]:size()):copy(inputs0[1])
+    inputs[2]:resize(inputs0[2]:size()):copy(inputs0[2])
+    target:resize(target0:size()):copy(target0)
     local batchSize = target:size(1)
 
     local feval = function(x)
@@ -100,14 +145,19 @@ function train()
     local x,fx = optim.sgd(feval,parameters,optimState)
     err = err + fx[1]
   end
-  print('Training error: '..err/max_iter)
+  print('Training error: '..err/display_iter)
 end
 
-train()
+stepsize = 30000
 
-if false then
-  m = nnf.ROIPooling(50,50):float()
-  o = m:forward(batches)
-  g = m:backward(batches,o)
+num_iter = 3000
+
+for i=1,num_iter do
+  print(('Iteration: %d/%d'):format(i,num_iter))
+  if i%(stepsize/display_iter) == 0 then
+    optimState.learningRate = optimState.learningRate/10
+  end
+
+  train()
+  
 end
-
