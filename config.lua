0adcd6757099ae8a5f50733b9b2dd902ce1d5ad7
Francisco Massa <fvsmassa@gmail.com>
2015-09-14 22:22:05 +0200

    Start cleaning data.lua and add a config file with parameters of the models

---
+++ config.lua
@@ -0,0 +1,101 @@
+
+local configs = {}
+
+local image_transformer_params = {
+  mean_pix={102.9801,115.9465,122.7717},
+  raw_scale = 255,
+  swap = {3,2,1}
+}
+
+configs.image_transformer_params = image_transformer_params
+configs.algo = {}
+
+--------------------------------------------------------------------------------
+-- RCNN
+--------------------------------------------------------------------------------
+
+local fp_params = {
+  crop_size  = 227,
+  padding    = 16,
+  use_square = false,
+}
+local bp_params = {
+  iter_per_batch = 100,
+  nTimesMoreData = 10,
+  batch_size = opt.batch_size,
+  fg_fraction = opt.fg_frac,
+  fg_threshold = 0.5,
+  bg_threshold = {0.0,0.5},
+  do_flip = true,
+  batch_dim = {3,fp_params.crop_size,fp_params.crop_size},
+}
+
+local RCNN = {
+  fp_params=fp_params,
+  bp_params=bp_params
+}
+
+configs.algo.RCNN = RCNN
+
+--------------------------------------------------------------------------------
+-- SPP
+--------------------------------------------------------------------------------
+
+local fp_params = {
+  scales = {480,576,688,874,1200},
+  randomscale = true,
+  sz_conv_standard = 13,
+  step_standard = 16,
+  offset0 = 21,
+  offset = 6.5,
+  inputArea = 224^2,
+}
+local num_chns = 256
+local pooling_scales = {{1,1},{2,2},{3,3},{6,6}}
+local pyr = torch.Tensor(pooling_scales):t()
+local pooled_size = pyr[1]:dot(pyr[2])
+local feat_dim = {num_chns*pooled_size}
+local bp_params = {
+  iter_per_batch = 500,
+  nTimesMoreData = 10,
+  batch_size = opt.batch_size,
+  fg_fraction = opt.fg_frac,
+  fg_threshold = 0.5,
+  bg_threshold = {0.1,0.5},
+  do_flip = true,
+  batch_dim = feat_dim,
+}
+
+local SPP = {
+  fp_params=fp_params,
+  bp_params=bp_params
+}
+
+configs.algo.SPP = SPP
+
+--------------------------------------------------------------------------------
+-- Fast-RCNN
+--------------------------------------------------------------------------------
+
+local fp_params = {
+  scale = {600}
+  max_size = 1000
+}
+local bp_params = {
+  imgs_per_batch = 2,
+  batch_size = opt.batch_size,
+  fg_fraction = opt.fg_frac,
+  fg_threshold = 0.5,
+  bg_threshold = {0.0,0.5},
+  do_flip = true,
+}
+
+local FRCNN = {
+  fp_params=fp_params,
+  bp_params=bp_params
+}
+
+configs.algo.FRCNN = FRCNN
+
+
+return configs
--- data.lua
+++ data.lua
@@ -101,45 +101,7 @@ else
   feat_provider_test.model = features
 end
 
---------------------------------------------------------------------------------
--- Compute conv5 feature cache (for SPP)
---------------------------------------------------------------------------------
-if opt.algo == 'SPP' then
-  print('Preparing conv5 features for '..ds_train.dataset_name..' '
-        ..ds_train.image_set)
-  local feat_cachedir = feat_provider.cachedir
-  for i=1,ds_train:size() do
-    xlua.progress(i,ds_train:size())
-    local im_name = ds_train.img_ids[i]
-    local cachefile = paths.concat(feat_cachedir,im_name)
-    if not paths.filep(cachefile..'.h5') then
-      local f = feat_provider:getConv5(i)
-    end
-    if not paths.filep(cachefile..'_flip.h5') then
-      local f = feat_provider:getConv5(i,true)
-    end
-    if i%50 == 0 then
-      collectgarbage()
-      collectgarbage()
-    end
-  end
-  
-  print('Preparing conv5 features for '..ds_test.dataset_name..' '
-        ..ds_test.image_set)
-  local feat_cachedir = feat_provider_test.cachedir
-  for i=1,ds_test:size() do
-    xlua.progress(i,ds_test:size())
-    local im_name = ds_test.img_ids[i]
-    local cachefile = paths.concat(feat_cachedir,im_name)
-    if not paths.filep(cachefile..'.h5') then
-      local f = feat_provider_test:getConv5(i)
-    end
-    if i%50 == 0 then
-      collectgarbage()
-      collectgarbage()
-    end
-  end
-end
+-- compute feature cache
 
 features = nil
 model = nil
